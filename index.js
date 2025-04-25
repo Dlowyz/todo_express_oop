88a4fa444c3bf54724c9df1a4ba7241b636f8bb2
Dlowyz <166107299+Dlowyz@users.noreply.github.com>
2025-04-25 10:59:53 +0300

    Create new Todo from user data

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 import express from 'express'
 import bodyParser from 'body-parser'
+import todoRoutes from './routes/todos.js'
 
 const app = express()
 const port = 3009
 
-app.use(bodyParser.json())
+app.use(express.json()); // JSON body jaoks
+app.use(express.urlencoded({ extended: true })); // form-urlencoded body jaoks
 
+app.use(bodyParser.json())
+app.use('/todos', todoRoutes)
 app.get('/json-test', (req, res) => {
     res.send({message: 'json oke'})
 })
